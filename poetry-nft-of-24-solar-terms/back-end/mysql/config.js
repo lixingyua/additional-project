08c315486fe5d16f9ecf1269bab92a24dcda343c
lixingyua <35121548+lixingyua@users.noreply.github.com>
2022-07-24 17:07:08 +0800

    Add files via upload

---
+++ poetry-nft-of-24-solar-terms/back-end/mysql/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const config = {
+    database: {
+        DATABASE: 'shici',
+        USERNAME: 'root',
+        PASSWORD: 'root',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
